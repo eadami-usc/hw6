97b8437cc797e85e5f4d37536a0dc97a6c10088c
Evan Adami <eadami@usc.edu>
2025-04-30 01:00:44 -0700

    Completed before testing

--- hash.h
+++ hash.h
@@ -16,19 +16,60 @@ struct MyStringHash {
             generateRValues();
         }
     }
+
     // hash function entry point (i.e. this is h(k))
     HASH_INDEX_T operator()(const std::string& k) const
     {
-        // Add your code here
-
-
+        unsigned long long w[5] = {0};
+        
+        // process <= 5 substrings of <= 6 characters each, starting from back
+        for (int substring = 0; substring < 5; substring++) {
+            // calculate starting position for this substring
+            int startPos = k.length() - 6 * (substring + 1);
+            if (startPos < 0) {
+                // break after processing entire string
+                if (startPos + 6 <= 0) {
+                    break;
+                }
+                startPos = 0;
+            }
+            
+            // process the current substring of <= 6 characters
+            unsigned long long val = 0;
+            int substringLength = 6;
+            if (startPos == 0) {
+                substringLength = k.length() - (6 * substring);
+            }
+            for (int i = 0; i < substringLength; i++) {
+                // base conversion
+                val = val * 36 + letterDigitToNumber(k[startPos + i]);
+            }
+            
+            // store these values in w array
+            w[4 - substring] = val;
+        }
+        
+        // apply the hash
+        HASH_INDEX_T hash = 0;
+        for (int i = 0; i < 5; i++) {
+            hash += rValues[i] * w[i];
+        }
+        
+        return hash;
     }
 
     // A likely helper function is to convert a-z,0-9 to an integral value 0-35
     HASH_INDEX_T letterDigitToNumber(char letter) const
     {
-        // Add code here or delete this helper function if you do not want it
-
+        HASH_INDEX_T val = 0;
+        if (letter >= 'a' && letter <= 'z') {
+            val = letter - 'a';
+        } else if (letter >= 'A' && letter <= 'Z') {
+            val = letter - 'A';
+        } else if (letter >= '0' && letter <= '9') {
+            val = letter - '0' + 26;
+        }
+        return val;
     }
 
     // Code to generate the random R values
--- ht.h
+++ ht.h
@@ -35,7 +35,7 @@ struct LinearProber : public Prober<KeyType> {
     {
         // Complete the condition below that indicates failure
         // to find the key or an empty slot
-        if( /* Fill me in */ ) {
+        if(this->numProbes_ >= this->m_) {
             return this->npos; 
         }
         HASH_INDEX_T loc = (this->start_ + this->numProbes_) % this->m_;
@@ -102,9 +102,12 @@ public:
     // To be completed
     HASH_INDEX_T next() 
     {
-
-
-
+        if(this->numProbes_ >= this->m_) {
+            return this->npos; 
+        }
+        HASH_INDEX_T loc = (this->start_ + this->numProbes_ * dhstep_) % this->m_;
+        this->numProbes_++;
+        return loc;
     }
 };
 
@@ -270,7 +273,10 @@ private:
     HASH_INDEX_T mIndex_;  // index to CAPACITIES
 
     // ADD MORE DATA MEMBERS HERE, AS NECESSARY
-
+    double resizeAlpha_;
+    size_t numItems_;
+    size_t numDeleted_;
+    size_t size_;
 };
 
 // ----------------------------------------------------------------------------
@@ -290,47 +296,62 @@ const HASH_INDEX_T HashTable<K,V,Prober,Hash,KEqual>::CAPACITIES[] =
 template<typename K, typename V, typename Prober, typename Hash, typename KEqual>
 HashTable<K,V,Prober,Hash,KEqual>::HashTable(
     double resizeAlpha, const Prober& prober, const Hasher& hash, const KEqual& kequal)
-       :  hash_(hash), kequal_(kequal), prober_(prober)
+       :  hash_(hash), kequal_(kequal), prober_(prober), mIndex_(0), resizeAlpha_(resizeAlpha), numItems_(0), numDeleted_(0), size_(CAPACITIES[0])
 {
-    // Initialize any other data members as necessary
-
+    table_.resize(size_, nullptr);
 }
 
 // To be completed
 template<typename K, typename V, typename Prober, typename Hash, typename KEqual>
 HashTable<K,V,Prober,Hash,KEqual>::~HashTable()
 {
-
+    for(HASH_INDEX_T i = 0; i < size_; i++) {
+        if(table_[i] != nullptr) {
+            delete table_[i];
+        }
+    }
 }
 
 // To be completed
 template<typename K, typename V, typename Prober, typename Hash, typename KEqual>
 bool HashTable<K,V,Prober,Hash,KEqual>::empty() const
 {
-
+    return numItems_ == 0;
 }
 
 // To be completed
 template<typename K, typename V, typename Prober, typename Hash, typename KEqual>
 size_t HashTable<K,V,Prober,Hash,KEqual>::size() const
 {
-
+    return numItems_;
 }
 
 // To be completed
 template<typename K, typename V, typename Prober, typename Hash, typename KEqual>
 void HashTable<K,V,Prober,Hash,KEqual>::insert(const ItemType& p)
 {
-
-
+    if ((numItems_ + numDeleted_) / size_ >= resizeAlpha_) {
+        this->resize();
+    }
+    HASH_INDEX_T loc = this->probe(p.first);
+    if(loc == npos) {
+        throw std::logic_error("No location available to insert item");
+    }
+    table_[loc] = new HashItem(p);
+    numItems_++;
 }
 
 // To be completed
 template<typename K, typename V, typename Prober, typename Hash, typename KEqual>
 void HashTable<K,V,Prober,Hash,KEqual>::remove(const KeyType& key)
 {
-
-
+    HashItem* item = this->internalFind(key);
+    if(item == nullptr) {
+        return;
+    }
+    item->deleted = true;
+    numDeleted_++;
+    numItems_--;
 }
 
 
@@ -404,8 +425,25 @@ typename HashTable<K,V,Prober,Hash,KEqual>::HashItem* HashTable<K,V,Prober,Hash,
 template<typename K, typename V, typename Prober, typename Hash, typename KEqual>
 void HashTable<K,V,Prober,Hash,KEqual>::resize()
 {
-
+    mIndex_++;
+    size_t newSize = CAPACITIES[mIndex_];
+    std::vector<HashItem*> oldTable = table_;
+    std::vector<HashItem*> newTable(newSize, nullptr);
+    table_ = newTable;
     
+    for(HASH_INDEX_T i = 0; i < size_; i++) {
+        if(oldTable[i] != nullptr) {
+            if (oldTable[i]->deleted) {
+                delete oldTable[i];
+            } else {
+                HASH_INDEX_T loc = this->probe(oldTable[i]->item.first);
+                table_[loc] = oldTable[i];
+            }
+        }
+    }
+
+    numDeleted_ = 0;
+    size_ = newSize;
 }
 
 // Almost complete
@@ -424,7 +462,7 @@ HASH_INDEX_T HashTable<K,V,Prober,Hash,KEqual>::probe(const KeyType& key) const
         }
         // fill in the condition for this else if statement which should 
         // return 'loc' if the given key exists at this location
-        else if(/* Fill me in */) {
+        else if(kequal_(key, table_[loc]->item.first)) {
             return loc;
         }
         loc = prober_.next();
